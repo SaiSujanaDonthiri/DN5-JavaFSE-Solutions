03753cea56dd24fa7fffdf8c776646074d3fd498
Sai Sujana <165762266+SaiSujanaDonthiri@users.noreply.github.com>
2026-06-04 00:45:24 +0530

    Add files via upload

---
+++ MODULE 3/Demo_37.java
@@ -0,0 +1,9 @@
+public class Demo_37 {
+    public void display() {
+        System.out.println("Hello Java");
+    }
+    public static void main(String[] args) {
+        Demo_37 d = new Demo_37();
+        d.display();
+    }
+}
---
+++ MODULE 3/Demo_38.java
@@ -0,0 +1,9 @@
+public class Demo_38{
+    public void display() {
+        System.out.println("Hello Java");
+    }
+    public static void main(String[] args) {
+        Demo_38 d = new Demo_38();
+        d.display();
+    }
+}
